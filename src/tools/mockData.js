2d055c8329d2f490cd15e399bdae93b38b9ba32c
codeblasterr <joinwithbernard@gmail.com>
2019-11-22 10:16:54 +0000

    Mock data initial setup check-in

---
+++ src/tools/mockData.js
@@ -0,0 +1,37 @@
+export const mockData = {
+    "passengerInfo" : [
+        /*{
+            "id": 123,
+            "name": "Anil",
+            "Age": 25,
+            "seatNo": "c4",
+            "specialMeals": true,
+            "withInfant": false,
+            "requireWheelChair": false,
+            "checkedIn": false,
+            "flightNo": 88674,
+            "address": {
+                "flatNo": 158,
+                "addressLine1": "",
+                "addressLine2": "",
+                "city": "",
+                "state": "",
+                "country": "",
+                "zipcode" ""
+            }
+        }*/
+    ],
+    "flightInfo": [
+        {
+            "number": 88675,
+            "name": "Indian airlines",
+            "scheduledTime": "20:45",
+            "ancilaryServices" : ["insurence", "Airport parking"]
+        },
+        {
+            "number": 88948,
+            "name": "Spice jet",
+            "scheduledTime": "09:58"
+        }
+    ]
+}
