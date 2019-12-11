d2ec1f6273f10921dc7391496fea0df87a3a3e32
Xavi <joinwithbernard@outlook.com>
2019-12-11 23:16:07 +0530

    Lazy loading implementation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { lazy } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navigation from "./components/Navigation/Navigation";
-import Home from "./containers/Home/Home";
-import AddOrUpdateUser from "./containers/AddOrUpdateUser/AddOrUpdateUser";
-import CheckIn from "./containers/CheckIn/CheckIn";
-import InFlight from "./containers/InFlight/InFlight";
-import Footer from "./components/Footer/Footer";
-import Login from "./containers/Login/Login";
+import ErrorBoundary from './containers/ErrorBoundary/ErrorBoundary';
 
 import "./App.scss";
+
+const Home = lazy(() => import("./containers/Home/Home"));
+const AddOrUpdateUser = lazy(() => import("./containers/AddOrUpdateUser/AddOrUpdateUser"));
+const CheckIn = lazy(() => import("./containers/CheckIn/CheckIn"));
+const InFlight = lazy(() => import("./containers/InFlight/InFlight"));
+const Footer = lazy(() => import("./components/Footer/Footer"));
+const Login = lazy(() => import("./containers/Login/Login"));
+const Navigation = lazy(() => import("./components/Navigation/Navigation"));
+
+const Fallback = () => {
+  return <div>Loading...</div>
+}
 
 const App = props => {
   return (
@@ -17,11 +23,15 @@ const App = props => {
       <div className="App">
         <Navigation {...props} />
         <main>
-          <Route path="/" exact component={Home} />
-          <Route path="/flights/check-in" component={CheckIn} />
-          <Route path="/flights/in-flight" component={InFlight} />
-          <Route path="/add-or-update-user" component={AddOrUpdateUser} />
-          <Route path="/login" component={Login} />
+          <ErrorBoundary>
+            <React.Suspense fallback={() => <Fallback />}>
+              <Route path="/" exact component={Home} />
+              <Route path="/flights/check-in" component={CheckIn} />
+              <Route path="/flights/in-flight" component={InFlight} />
+              <Route path="/add-or-update-user" component={AddOrUpdateUser} />
+              <Route path="/login" component={Login} />
+            </React.Suspense>
+          </ErrorBoundary>
         </main>
         <Footer />
       </div>
