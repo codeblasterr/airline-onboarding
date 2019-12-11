5cf226a66548d5ce9d9e2548a5806be732e95b2f
codeblasterr <joinwithbernard@gmail.com>
2019-12-11 18:23:40 +0530

    Seats
    Seats

---
+++ src/components/Seats/Seat/Seat.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Seat = props => {
+  return <div className="cls_seat">{props.seatNo}</div>;
+};
+
+export default Seat;
--- src/components/Seats/Seats.js
+++ src/components/Seats/Seats.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import './Seats.scss';
+import Seat from "./Seat/Seat";
+
+import "./Seats.scss";
 
 const Seats = () => {
-    let seats = [];
-    let alpha = ['A', 'B', 'C'];
-    for(let counter = 0; counter < alpha.length; ++counter) {
-        let row = []
-        for(let index = 1; index <= 10; ++index) {
-            row.push(<div className="cls_seat">{alpha[counter] + index}</div>);
-        }
-        seats.push(<div className="cls_seatRow">{row}</div>);
+  let seats = [];
+  let alpha = ["A", "B", "C"];
+  for (let counter = 0; counter < alpha.length; ++counter) {
+    let row = [];
+    for (let index = 1; index <= 10; ++index) {
+      row.push(
+        <Seat key={alpha[counter] + index} seatNo={alpha[counter] + index} />
+      );
     }
-    return (
-        <div className="cls_seatCont">
-            {seats}
-        </div>
-    );
-}
+    seats.push(<div className="cls_seatRow">{row}</div>);
+  }
+  return <div className="cls_seatCont">{seats}</div>;
+};
 
 export default Seats;
